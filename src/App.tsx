b8a0164c29363273ac9bf1943dccdb63ce00320c
Kelvin Pires <pireskelvin7@gmail.com>
2022-07-27 00:42:23 -0300

    Create home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Menubar } from "./components/Menubar";
+import { Searchbar } from "./components/Searchbar";
+import { HomePage } from "./pages/HomePage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Menubar />
+      <Searchbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
