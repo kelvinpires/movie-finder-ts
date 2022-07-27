c558a867a7def983298e066901a91af58a653a3a
Kelvin Pires <pireskelvin7@gmail.com>
2022-07-27 00:42:46 -0300

    Add context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalContextProvider } from "./context/GlobalContext";
+import { GlobalStyles } from "./GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GlobalContextProvider>
+      <GlobalStyles />
+      <App />
+    </GlobalContextProvider>
   </React.StrictMode>
 );
