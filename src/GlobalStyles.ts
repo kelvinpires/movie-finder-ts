f9425dcaede20b020dcf9ac3d80ac1bfddbb49fd
Kelvin Pires <pireskelvin7@gmail.com>
2022-07-27 00:38:45 -0300

    Create global styles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        padding-left: 10rem;
+        background-color: var(--body);
+    }
+
+    html {
+        font-size: 62.5%;
+        font-family: 'Roboto', sans-serif;
+
+        --white: #fff;
+        --gray: #999;
+        --black: #000;
+        --body: #1f1f1e;
+        --yellow: #FFCB47
+    }
+`;
