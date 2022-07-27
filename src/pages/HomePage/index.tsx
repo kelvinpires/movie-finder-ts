f13c54fcf4c8bf49ad2c3320e4e7b86ed96c4776
Kelvin Pires <pireskelvin7@gmail.com>
2022-07-27 16:02:22 -0300

    Add carousel

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,14 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { Banner } from "../../components/Banner";
+import { Carousel } from "../../components/Carousel";
 import { GlobalContext } from "../../context/GlobalContext";
+import { MoviesType } from "../../types/MoviesType";
 
 export const HomePage = () => {
-  const { trending } = useContext(GlobalContext);
+  const { trending, getCategory } = useContext(GlobalContext);
+
+  const [popularMovies, setPopularMovies] = useState<MoviesType[]>([]);
+  const [popularSeries, setPopularSeries] = useState<MoviesType[]>([]);
+
+  useEffect(() => {
+    getCategory("movie", "popular", setPopularMovies);
+    getCategory("tv", "popular", setPopularSeries);
+  }, []);
 
   return (
     <>
       <Banner content={trending} />
+      {popularMovies && (
+        <Carousel content={popularMovies} subtitle="Filmes populares" />
+      )}
+      {popularSeries && (
+        <Carousel content={popularSeries} subtitle="Séries populares" />
+      )}
     </>
   );
 };
