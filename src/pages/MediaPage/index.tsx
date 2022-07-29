1602e401c5e98ad6af1a75c3af43d57939dbbb2a
Kelvin Pires <pireskelvin7@gmail.com>
2022-07-29 01:08:31 -0300

    Create MediaPage

---
+++ src/pages/MediaPage/index.tsx
@@ -0,0 +1,166 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Banner } from "../../components/Banner";
+import { GlobalContext } from "../../context/GlobalContext";
+import { MoviesType } from "../../types/MoviesType";
+import {
+  DescriptionWrapper,
+  Details,
+  Image,
+  InfoLabel,
+  InfoLi,
+  InfoUl,
+  InfoValue,
+  Overview,
+  PosterWrapper,
+  Subtitle,
+  Title,
+} from "./styles";
+
+export const MediaPage = () => {
+  const [content, setContent] = useState<MoviesType[]>([]);
+  const { getDetails } = useContext(GlobalContext);
+
+  const { media_type, id } = useParams();
+
+  useEffect(() => {
+    getDetails(media_type!, Number(id)!, setContent);
+  }, []);
+
+  return (
+    <>
+      <Banner content={content} />
+      {content.length > 0 && (
+        <>
+          {content.map((item) => {
+            const {
+              title,
+              name,
+              overview,
+              release_date,
+              poster_path,
+              runtime,
+              id,
+              budget,
+              revenue,
+              production_companies,
+              first_air_date,
+              genres,
+              created_by,
+            } = item;
+
+            // date
+            const date = release_date
+              ? new Date(release_date).toLocaleDateString()
+              : new Date(first_air_date).toLocaleDateString();
+
+            // runtime
+            const totalHours = runtime / 60;
+            const hour = Math.floor(totalHours);
+            const minutes = Math.round((totalHours - hour) * 60);
+            const time = `${hour}h ${minutes}min`;
+
+            return (
+              <Details key={id}>
+                <PosterWrapper>
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w370_and_h556_bestv2${poster_path}`}
+                    alt={`Pôster de ${title || name}`}
+                  />
+                </PosterWrapper>
+                <DescriptionWrapper>
+                  <Title>{title || name}</Title>
+
+                  <Subtitle>Sinopse</Subtitle>
+                  <Overview>{overview}</Overview>
+                  <InfoUl>
+                    <InfoLi>
+                      <InfoLabel>Data de lançamento</InfoLabel>
+                      <InfoValue>{date}</InfoValue>
+                    </InfoLi>
+                    {created_by && (
+                      <InfoLi>
+                        <InfoLabel>Criado por</InfoLabel>
+                        <InfoValue>
+                          {created_by.map((person) => (
+                            <Link
+                              key={person.id}
+                              style={{ color: "#fff" }}
+                              to={`/person/${person.id}`}
+                            >
+                              {person.name}
+                            </Link>
+                          ))}
+                        </InfoValue>
+                      </InfoLi>
+                    )}
+                    {runtime && (
+                      <InfoLi>
+                        <InfoLabel>Duração</InfoLabel>
+                        <InfoValue>{time}</InfoValue>
+                      </InfoLi>
+                    )}
+                    {budget > 0 && (
+                      <InfoLi>
+                        <InfoLabel>Orçamento</InfoLabel>
+                        <InfoValue>
+                          {budget.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "usd",
+                          })}
+                        </InfoValue>
+                      </InfoLi>
+                    )}
+                    {revenue > 0 && (
+                      <InfoLi>
+                        <InfoLabel>Receita</InfoLabel>
+                        <InfoValue>
+                          {revenue.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "usd",
+                          })}
+                        </InfoValue>
+                      </InfoLi>
+                    )}
+                    {genres.length > 0 && (
+                      <InfoLi>
+                        <InfoLabel>Gêneros</InfoLabel>
+                        <InfoValue>
+                          {genres.map((genre) => (
+                            <Link
+                              key={genre.id}
+                              style={{ color: "#fff" }}
+                              to={`/category/${genre.id}`}
+                            >
+                              {genre.name}
+                            </Link>
+                          ))}
+                        </InfoValue>
+                      </InfoLi>
+                    )}
+                    {production_companies.length > 0 && (
+                      <InfoLi>
+                        <InfoLabel>Produções</InfoLabel>
+                        <InfoValue>
+                          {production_companies.map((production) => (
+                            <Link
+                              key={production.id}
+                              style={{ color: "#999" }}
+                              to="#"
+                            >
+                              {production.name}
+                            </Link>
+                          ))}
+                        </InfoValue>
+                      </InfoLi>
+                    )}
+                  </InfoUl>
+                </DescriptionWrapper>
+              </Details>
+            );
+          })}
+        </>
+      )}
+    </>
+  );
+};
---
+++ src/pages/MediaPage/styles.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Details = styled.section`
+  padding: 10rem 5rem 2rem;
+  display: flex;
+  align-items: flex-start;
+  gap: 2rem;
+`;
+
+export const PosterWrapper = styled.div`
+  min-width: 37rem;
+  min-height: 55.6rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const DescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 3.2rem;
+  font-weight: 400;
+  color: var(--white);
+`;
+
+export const Subtitle = styled.span`
+  font-size: 2.6rem;
+  font-weight: 500;
+  color: var(--white);
+  margin-top: 3rem;
+`;
+
+export const Overview = styled.p`
+  font-size: 1.6rem;
+  color: var(--white);
+  font-weight: 400;
+  line-height: 1.6;
+`;
+
+export const InfoUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  margin-top: 4rem;
+  list-style: none;
+  gap: 2rem;
+`;
+
+export const InfoLi = styled.li`
+  font-size: 1.6rem;
+  display: flex;
+  align-items: center;
+`;
+
+export const InfoLabel = styled.span`
+  color: var(--white);
+  flex: 1;
+  margin-right: 1rem;
+`;
+
+export const InfoValue = styled.span`
+  color: var(--white);
+  flex: 4;
+  display: flex;
+  gap: 1rem;
+`;
