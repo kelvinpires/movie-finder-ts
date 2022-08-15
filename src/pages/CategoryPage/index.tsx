dd98e3e7b140aa4351e9c203b9c379d8ed20fa6f
Kelvin Pires <pireskelvin7@gmail.com>
2022-08-15 00:54:43 -0300

    Create category page

---
+++ src/pages/CategoryPage/index.tsx
@@ -0,0 +1,107 @@
+import { Star, StarHalf } from "phosphor-react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Banner } from "../../components/Banner";
+import { GlobalContext } from "../../context/GlobalContext";
+import { MoviesType } from "../../types/MoviesType";
+import {
+  Card,
+  Container,
+  ContentWrapper,
+  Genre,
+  GenresWrapper,
+  PosterImg,
+  PosterWrapper,
+  Redirect,
+  Span,
+} from "./styles";
+
+export const CategoryPage = () => {
+  const [content, setContent] = useState<MoviesType[]>([]);
+  const [contentByGenre, setContentByGenre] = useState<MoviesType[]>([]);
+  const [genres, setGenres] = useState<Array<{ id: number; name: string }>>([]);
+  const [activeGenres, setActiveGenres] = useState<number[]>([]);
+
+  const { getCategory, getGenres, getContentByGenre } =
+    useContext(GlobalContext);
+
+  const { type } = useParams();
+
+  useEffect(() => {
+    setContent([]);
+    setActiveGenres([]);
+
+    getCategory(type!, "popular", setContent);
+    getContentByGenre(type!, [], setContentByGenre);
+
+    getGenres(type!, setGenres);
+  }, [type]);
+
+  useEffect(() => {
+    setContentByGenre([]);
+    getContentByGenre(type!, activeGenres, setContentByGenre);
+  }, [activeGenres]);
+
+  function handleGenres(genreId: number) {
+    if (!activeGenres.includes(genreId)) {
+      setActiveGenres((prev) => [...prev, genreId]);
+    } else {
+      const genresFilter = activeGenres.filter((genre) => genre != genreId);
+      setActiveGenres(genresFilter);
+    }
+  }
+
+  return (
+    <>
+      <Banner content={content.slice(0, 7)} />
+      <Container>
+        <GenresWrapper>
+          {genres.map((genre) => {
+            const isGenreActive = activeGenres.includes(genre.id);
+
+            return (
+              <Genre
+                isActive={isGenreActive}
+                onClick={() => handleGenres(genre.id)}
+                key={genre.id}
+              >
+                {genre.name}
+              </Genre>
+            );
+          })}
+        </GenresWrapper>
+        <ContentWrapper>
+          {contentByGenre.map((content) => {
+            const { id, media_type, poster_path, title, name, vote_average } =
+              content;
+
+            return (
+              <Card key={id}>
+                <Redirect to={`/${media_type}/${id}`}>
+                  <PosterWrapper>
+                    <PosterImg
+                      loading="lazy"
+                      src={`https://image.tmdb.org/t/p/w342${poster_path}`}
+                      alt={title || name}
+                    />
+                  </PosterWrapper>
+                  <Span>{title || name}</Span>
+                  {vote_average && (
+                    <Span>
+                      {vote_average < 6 ? (
+                        <StarHalf color="#FFCB47" size={24} weight="fill" />
+                      ) : (
+                        <Star color="#FFCB47" size={24} weight="fill" />
+                      )}
+                      {vote_average?.toFixed(1)}
+                    </Span>
+                  )}
+                </Redirect>
+              </Card>
+            );
+          })}
+        </ContentWrapper>
+      </Container>
+    </>
+  );
+};
---
+++ src/pages/CategoryPage/styles.ts
@@ -0,0 +1,97 @@
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+  padding: 5rem;
+`;
+
+export const GenresWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem 1rem;
+`;
+
+type GenreProps = {
+  isActive: boolean;
+};
+
+export const Genre = styled.span`
+  border: 0.2rem solid var(--gray);
+  color: var(--white);
+  border-radius: 0.5rem;
+  font-size: 1.6rem;
+  font-weight: 500;
+  padding: 1rem 2rem;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  ${({ isActive }: GenreProps) =>
+    isActive &&
+    css`
+      background-color: var(--white);
+      color: var(--black);
+      border-color: var(--white);
+    `}
+`;
+
+export const ContentWrapper = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem 1rem;
+  margin: 10rem 0;
+`;
+
+export const Card = styled.li`
+  width: 25rem;
+  min-height: 35rem;
+  margin-bottom: 1rem;
+
+  @keyframes animate {
+    0% {
+      opacity: 0;
+      visibility: hidden;
+    }
+    50% {
+      opacity: 0.5;
+      visibility: visible;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  animation: animate 0.25s ease-in;
+`;
+
+export const Redirect = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  text-decoration: none;
+`;
+
+export const PosterWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  transition: all 0.25s ease-in-out;
+
+  &:hover {
+    transform: scale(1.03);
+  }
+`;
+
+export const PosterImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const Span = styled.span`
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: var(--white);
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
