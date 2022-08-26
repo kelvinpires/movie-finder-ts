30d3ed7dc1db0efdec72152d601316b55a516424
Kelvin Pires <pireskelvin7@gmail.com>
2022-08-26 16:02:36 -0300

    Add useQuery hook

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,26 +1,42 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 import { Banner } from "../../components/Banner";
 import { Carousel } from "../../components/Carousel";
 import { GlobalContext } from "../../context/GlobalContext";
 import { ContentResponse } from "../../types/MoviesType";
 
+import { useQuery } from "@tanstack/react-query";
+
 export const HomePage = () => {
   const { getCategory, getTrending } = useContext(GlobalContext);
 
-  const [trending, setTrending] = useState<ContentResponse[]>([]);
-  const [popularMovies, setPopularMovies] = useState<ContentResponse[]>([]);
-  const [popularSeries, setPopularSeries] = useState<ContentResponse[]>([]);
+  const { data: trending, isLoading } = useQuery<ContentResponse[]>(
+    ["trending"],
+    async () => {
+      const trendingContent: ContentResponse[] = await getTrending()!;
 
-  useEffect(() => {
-    getCategory("movie", "popular", setPopularMovies);
-    getCategory("tv", "popular", setPopularSeries);
-    getTrending(setTrending);
-  }, []);
+      return trendingContent;
+    }
+  );
+
+  const { data: popularMovies } = useQuery<ContentResponse[]>(
+    ["movies"],
+    () => {
+      const category = getCategory("movie", "popular")!;
+
+      return category;
+    }
+  );
+  const { data: popularSeries } = useQuery<ContentResponse[]>(["tv"], () => {
+    const category = getCategory("tv", "popular")!;
+
+    return category;
+  });
 
   return (
     <>
-      <Banner content={trending} />
+      {!isLoading && <Banner content={trending!} />}
+
       {popularMovies && (
         <Carousel content={popularMovies} subtitle="Filmes populares" />
       )}
