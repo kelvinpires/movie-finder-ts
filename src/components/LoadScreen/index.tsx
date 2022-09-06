357fce5314862eabf71930092d5dcf2c62418532
Kelvin Pires <pireskelvin7@gmail.com>
2022-09-06 00:23:31 -0300

    Create LoadScreen

---
+++ src/components/LoadScreen/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Content, Load } from "./styles";
+
+export const LoadScreen = () => {
+  return (
+    <Load>
+      <Container>
+        <Content></Content>
+        <Content></Content>
+        <Content></Content>
+        <Content></Content>
+      </Container>
+    </Load>
+  );
+};
---
+++ src/components/LoadScreen/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Load = styled.div`
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--body);
+`;
+
+export const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+`;
+
+export const Content = styled.div`
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+
+  &:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  &:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  &:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
